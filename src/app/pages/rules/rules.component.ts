d845ca27c00abd174497956a1ebaa45c44df37d4
warren <1322641861@qq.com>
2022-12-30 16:29:11 +0800

    完成玩法规则

--- src/app/pages/rules/rules.component.ts
+++ src/app/pages/rules/rules.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rules.component.scss']
 })
 export class RulesComponent implements OnInit {
+  ticketMsg: any[] = [];
+  introduceMsg: any[] = [];
+  gameMsg: any[] = [];
+  tabIndex = 0;
+  navList = [
+    '介绍问答',
+    '奖券规则',
+    '游戏规范',
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getDescrtion();
   }
 
+  getDescrtion() {
+    fetch("/assets/other/lottery-desc.json")
+      .then((res) => res.json())
+      .then((json: Rules) => {
+          if (json) {
+            this.ticketMsg = json.ticket.split('\n');
+            this.introduceMsg = json.introduce.split('\n');
+            this.gameMsg = json.game.split('\n');
+          }
+      });
+  }
+
+  changeTabIndex(index: number) {
+    this.tabIndex = index;
+  }
+
+}
+
+interface Rules {
+  game: string;
+  introduce: string;
+  ticket: string;
 }
