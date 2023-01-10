4cbddc8a80fcd6e1bcdcfbade0f6b0234fb7c36f
warren <1322641861@qq.com>
2023-01-10 20:55:48 +0800

    h5視頻彈窗

--- src/app/components/mobile/h5-video-modal/h5-video-modal.component.ts
+++ src/app/components/mobile/h5-video-modal/h5-video-modal.component.ts
@@ -1,20 +1,58 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-h5-video-modal',
   templateUrl: './h5-video-modal.component.html',
   styleUrls: ['./h5-video-modal.component.scss']
 })
-export class H5VideoModalComponent implements OnInit {
+export class H5VideoModalComponent implements OnInit, OnChanges {
   @Input() openModal = false;
+  @Input() issue = '2022355';
+  @Input() openTime = '2022-12-26 21:34:15';
+  @Input() codeList: any[] = [6,5,3,7,3];
+  @Input() modalSrc = '';
+  @Input() vid = '';
   @Output() changeModalFn = new EventEmitter();
+  modalClassName = '';
+  playing = false;
+  videoDom: any;
+  listened = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.modalClassName = 'modal-video' + this.vid;
+  }
+
   changeModal() {
+    this.removeListener();
     this.changeModalFn.emit();
+  }
+
+  playVideo() {
+    this.videoDom = this.videoDom ? this.videoDom : document.getElementsByClassName(this.modalClassName)[0];
+    this.videoDom.play();
+    this.playing = true;
+    this.pauseListener();
+  }
+
+  pauseListener() {
+    if (!this.listened) {
+      this.videoDom.addEventListener('pause', () => {
+        this.playing = false;
+      });
+    }
+    this.listened = true;
+  }
+
+  removeListener() {
+    if (this.videoDom) {
+      this.videoDom.removeEventListener('pause', () => {console.log('我被移除了')})
+      this.videoDom = null;
+    }
+    this.listened = false;
   }
 
 }
--- src/app/pages/mobile/h5videos/h5videos.component.ts
+++ src/app/pages/mobile/h5videos/h5videos.component.ts
@@ -10,6 +10,8 @@ export class H5videosComponent implements OnInit, AfterViewInit {
   poster = 'assets/images/background/play_bg.jpg';
   videoList: any[] = [];
   openModal = false;
+  modalSrc = '';
+  modalVId = '';
   constructor() { }
 
   ngOnInit(): void {
@@ -22,7 +24,15 @@ export class H5videosComponent implements OnInit, AfterViewInit {
   searchEvent(issueNo: any) {
     console.log(issueNo);
   }
-  changeModal() {
+
+  changeModal(videoItem: any = null) {
+    if (videoItem) {
+      this.modalSrc = videoItem.src;
+      this.modalVId = videoItem.id;
+    } else {
+      this.modalSrc = '';
+      this.modalVId = '';
+    }
     this.openModal = !this.openModal;
   }
 
@@ -38,7 +48,7 @@ export class H5videosComponent implements OnInit, AfterViewInit {
         id: 2,
         issue: '2022353',
         poster: 'assets/images/background/play_bg.jpg',
-        src: '//player.alicdn.com/video/aliyunmedia.mp4'
+        src: 'https://my-files-1259410276.cos.ap-chengdu.myqcloud.com/media/美人谷.mp4'
       },
       {
         id: 3,
@@ -50,7 +60,7 @@ export class H5videosComponent implements OnInit, AfterViewInit {
         id: 4,
         issue: '2022353',
         poster: 'assets/images/background/play_bg.jpg',
-        src: '//player.alicdn.com/video/aliyunmedia.mp4'
+        src: 'https://my-files-1259410276.cos.ap-chengdu.myqcloud.com/media/美人谷.mp4'
       },
       {
         id: 5,
@@ -62,7 +72,7 @@ export class H5videosComponent implements OnInit, AfterViewInit {
         id: 6,
         issue: '2022353',
         poster: 'assets/images/background/play_bg.jpg',
-        src: '//player.alicdn.com/video/aliyunmedia.mp4'
+        src: 'https://my-files-1259410276.cos.ap-chengdu.myqcloud.com/media/美人谷.mp4'
       }
     ];
   }
