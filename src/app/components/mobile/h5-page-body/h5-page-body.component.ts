eb6bab8a882eb4fd14c2a4a75c8f6b897b03dfe1
warren <1322641861@qq.com>
2023-01-11 14:59:40 +0800

    下拉刷新

--- src/app/components/mobile/h5-page-body/h5-page-body.component.ts
+++ src/app/components/mobile/h5-page-body/h5-page-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-h5-page-body',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./h5-page-body.component.scss']
 })
 export class H5PageBodyComponent implements OnInit {
+  @Input() direction = '';
+  @Input() hasRefresh = false;
+  @Output() upRefresh = new EventEmitter();
+  @Output() downRefresh = new EventEmitter();
+
+  state = {
+    refreshState: {
+      currentState: 'deactivate',
+      drag: false
+    },
+    endReachedRefresh: false, // 滚到底部自动刷新, 适合长滚动列表
+  };
+  dtPullToRefreshStyle = { height: '100%' };
+  page = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  pullToRefresh(event: any) {
+    if (event === 'down') {
+      this.downRefresh.emit();
+    } else if (event === 'up') {
+      this.upRefresh.emit();
+    } else if (event === 'endReachedRefresh') {}
   }
 
 }
