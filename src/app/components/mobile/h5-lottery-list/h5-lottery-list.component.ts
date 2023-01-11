621da6e84c2c55f3a05c9b9c6b297bc15e937669
warren <1322641861@qq.com>
2023-01-11 11:26:09 +0800

    \(^o^)/~

--- src/app/components/mobile/h5-lottery-list/h5-lottery-list.component.ts
+++ src/app/components/mobile/h5-lottery-list/h5-lottery-list.component.ts
@@ -7,8 +7,22 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class H5LotteryListComponent implements OnInit {
   @Input() lotteryHistory: any[] = [];
+  openModal = false;
+  modalSrc = '';
+  modalVId = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeModal(videoItem: any = null) {
+    if (videoItem) {
+      this.modalSrc = videoItem.src;
+      this.modalVId = videoItem.videoId;
+    } else {
+      this.modalSrc = '';
+      this.modalVId = '';
+    }
+    this.openModal = !this.openModal;
   }
 }
--- src/app/pages/mobile/h5home/h5home.component.ts
+++ src/app/pages/mobile/h5home/h5home.component.ts
@@ -28,16 +28,16 @@ export class H5homeComponent implements OnInit {
   getHistoryList() {
     setTimeout(() => {
       this.lotteryHistory = [
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
-        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 11},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 1, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 2, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 3, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 4, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 5, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 6, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 7, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 8, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 9, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
+        {issue: '2022355', openTime: '2022-12-22 21:20:00', code: ['8', '2', '6', '3', '8'], videoId: 10, src: '//player.alicdn.com/video/aliyunmedia.mp4'},
       ];
     }, 1000);
   }
